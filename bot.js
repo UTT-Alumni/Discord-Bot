702a27c9cf41acda9590d6a606f722d158e4f619
Azaor <alexandre0lambdinnov.fr>
2022-10-09 12:10:20 +0200

    refactor: JS Bot V1 functionnal
    - REFACTOR : Main code extracted from start
    - FEAT : OnMemberAdd message sent

--- bot.js
+++ bot.js
@@ -1,91 +1,134 @@
-const { Client, GatewayIntentBits } = require('discord.js')
-const { Modal, TextInputComponent, showModal } = require('discord-modals')
-
+const {
+  Client,
+  GatewayIntentBits,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ModalBuilder,
+  TextInputBuilder,
+} = require('discord.js')
 
 require('dotenv').config()
 
 const messages = require('./messages.json')
 
-const start = async () => {
-  const client = new Client({
-    intents: [
-      GatewayIntentBits.Guilds,
-      // GatewayIntentBits.GuildMembers,
-      // GatewayIntentBits.GuildMembers,
-      GatewayIntentBits.GuildMembers,
-      // GatewayIntentBits.MessageContent
-    ],
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    // GatewayIntentBits.GuildMembers,
+    // GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMembers,
+    // GatewayIntentBits.MessageContent
+  ],
+})
+
+const modal = new ModalBuilder().setCustomId('utt-alumni-bot-id').setTitle('Modal')
+const firstNameComponent = new ActionRowBuilder().addComponents(new TextInputBuilder()
+  .setCustomId('firstName')
+  .setLabel('Prénom')
+  .setStyle('Short')
+  .setMinLength(1)
+  .setMaxLength(16)
+  .setPlaceholder('Écrivez votre prénom')
+  .setRequired(true))
+const nameComponent = new ActionRowBuilder().addComponents(new TextInputBuilder()
+  .setCustomId('name')
+  .setLabel('Nom de famille')
+  .setStyle('Short')
+  .setPlaceholder('Écrivez votre nom de famille')
+  .setRequired(true))
+const graduationYearComponent = new ActionRowBuilder().addComponents(new TextInputBuilder()
+  .setCustomId('graduationYear')
+  .setLabel('Année de diplôme')
+  .setStyle('Short')
+  .setMinLength(4)
+  .setMaxLength(4)
+  .setPlaceholder('Votre année de diplôme (espérée si vous êtes étudiant)')
+  .setRequired(true))
+const educationComponent = new ActionRowBuilder().addComponents(new TextInputBuilder()
+  .setCustomId('education')
+  .setLabel('Branche')
+  .setStyle('Short')
+  .setMinLength(2)
+  .setMaxLength(5)
+  .setPlaceholder('Branche ingénieur, master ou doctorat')
+  .setRequired(true))
+
+modal.addComponents([
+  firstNameComponent,
+  nameComponent,
+  graduationYearComponent,
+  educationComponent,
+])
+async function createForm(interaction) {
+  await interaction.showModal(modal)
+}
+
+async function onUserAdded(member) {
+  // Generates the name of the channel according to channelID provided in config.json file
+  const channel = client.channels.cache.get(process.env.CHANNEL_ID)
+
+  // Sends custom message mentioning the user and adds rules provided in config.json file
+  const row = new ActionRowBuilder().addComponents(
+    new ButtonBuilder()
+      .setCustomId('primary')
+      .setLabel('Je me présente !')
+      .setStyle(ButtonStyle.Primary),
+  )
+  // Private message TODO: Maybe not use this
+  await channel.send({
+    content: messages.fr.welcome,
+    components: [row],
+    ephemeral: true,
   })
+}
+/**
+ * Validate the modal
+ *
+ * @param {BaseInteraction} interaction Interaction data
+ */
+async function onModalSubmit(interaction) {
+  if (interaction.customId === 'utt-alumni-bot-id') {
+    const firstName = interaction.fields.getTextInputValue('firstName')
+    const name = interaction.fields.getTextInputValue('name')
+    const graduationYear = interaction.fields.getTextInputValue('graduationYear')
+    const education = interaction.fields.getTextInputValue('education')
+    let fullName = `${firstName} ${name} - ${graduationYear} ${education}`
+    // Check if the full name can be handle by discord
+    if (fullName.length > 32) {
+      let toReduce = fullName.length - 31 // Because the name will have "."
+      let newName = name.slice(0, name.length - toReduce + 1)
+      let newFirstName = firstName
+      // If the name reduction is not enough
+      if (name.length - toReduce < 0) {
+        toReduce -= name.length + 1 // Because the firstname will have a "."
+        newName = `${name.slice(0, 1)}`
+        newFirstName = `${firstName.slice(0, firstName.length - toReduce)}.`
+      }
+      fullName = `${newFirstName} ${newName}. - ${graduationYear} ${education}`
+    }
+    interaction.member.setNickname(fullName)
+    const myRole = interaction.member.guild.roles.cache.find(
+      (role) => role.name === process.env.ROLE_TO_ATTRIBUTE,
+    )
+    interaction.member.roles.add(myRole.id)
+  }
+}
+
+const start = async () => {
   client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
-    client.user.setActivity("for new users to join", {type : "WATCHING"});
+    client.user.setActivity('for new users to join', { type: 'WATCHING' })
   })
 
-  client.on('createInteraction', async (interaction) => {
-    const modal = new Modal()
-        .setCustomId('utt-alumni-bot-id')
-        .setTitle('Modal')
-    const firstNameComponent = new TextInputComponent()
-        .setCustomId('firstName')
-        .setLabel('Prénom')
-        .setStyle('SHORT')
-        .setPlaceholder('Écrivez votre prénom')
-        .setRequired(true)
-    const nameComponent = new TextInputComponent()
-        .setCustomId('name')
-        .setLabel('Nom de famille')
-        .setStyle('SHORT')
-        .setPlaceholder('Écrivez votre nom de famille')
-        .setRequired(true)
-    const graduationYearComponent = new TextInputComponent()
-        .setCustomId('graduationYear')
-        .setLabel('Année de diplôme')
-        .setStyle('SHORT')
-        .setPlaceholder('Votre année de diplôme (espérée si vous êtes étudiant)')
-        .setRequired(true)
-    const educationComponent = new TextInputComponent()
-        .setCustomId('education')
-        .setLabel('Branche')
-        .setStyle('SHORT')
-        .setPlaceholder('Branche ingénieur, master ou doctorat')
-        .setRequired(true)
-    modal.addComponents([firstNameComponent, nameComponent, graduationYearComponent, educationComponent])
-    await showModal(modal, {
-      client, // Client to show the Modal through the Discord API.
-      interaction, // Show the modal with interaction data.
-    })
-  })
+  client.on('guildMemberAdd', onUserAdded)
 
-  client.on('guildMemberAdd', async (member) => {
-    console.log(member)
-    console.log(member.user.username + " has joined the server! ");
-
-    // Generates the name of the server according to serverID provided in config.json file
-    const serverName = client.guilds.cache.get(process.env.SERVER_ID);
-    // Generates the name of the channel according to channelID provided in config.json file
-    const channel = client.channels.cache.get(process.env.CHANNEL_ID)
-
-    // Sends custom message mentioning the user and adds rules provided in config.json file
-    channel.send(`Hello, ${member.user} welcome to ${serverName}!\n`); // TODO: send message with button
-  })
-
-  client.on('modalSubmit', async (submitModal) => {
-    console.log('utt-alumni-bot', submitModal)
-    if (submitModal.customId === 'utt-alumni-bot-id') {
-      const firstName = submitModal.getTextInputValue('firstName')
-      const name = submitModal.getTextInputValue('name')
-      const graduationYear = submitModal.getSelectMenuValues('graduationYear')
-      const education = submitModal.getSelectMenuValues('education')
-
-      console.log(firstName, name, graduationYear, education)
-      // TODO: change nickname with max length
-      submitModal.member.setNickname(`${firstName} ${name} - ${graduationYear} ${education}`)
-      const myRole = submitModal.member.guild.roles.cache.find(role => role.name === process.env.ROLE_ID);
-      submitModal.member.roles.add(myRole.id);
-
-      await submitModal.reply(messages.fr.welcome)
-
-
+  client.on('interactionCreate', async (interaction) => {
+    if (interaction.isButton()) {
+      createForm(interaction)
+    }
+    if (interaction.isModalSubmit()) {
+      onModalSubmit(interaction)
     }
   })
 
